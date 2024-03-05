396845ec0a5503a889fa6670fc1bf48950464d73
Chayan <chayan.surana3@gmail.com>
2024-03-05 23:17:37 +0530

    new changes

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -7,9 +7,8 @@ export default function Solution({ quiz, score, userAnswers }) {
 
     return (
         <div className="max-w-3xl mx-auto">
-            <div className="flex justify-between items-center mb-4">
-                <h2 className="text-right">Your Score: {score}</h2>
-            </div>            {quiz.questions.map((question, index) => (
+            <h2 className="right-0 mt-2 text-lg">Your Score: {score}</h2>
+            {quiz.questions.map((question, index) => (
                 <div key={index} className="bg-white shadow-md mt-4 text-black rounded-md p-6 mb-4">
                     <h3 className="text-xl font-semibold mb-4">Question {index + 1}</h3>
                     <h4 className="text-lg mb-4">{question.question}</h4>
@@ -28,7 +27,7 @@ export default function Solution({ quiz, score, userAnswers }) {
                             </button>
                         ))}
                     </div>
-                    {index >= quiz.questions.length - 1 && <div className="mt-4"><button onClick={goToHome} className="text-center mx-auto px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded shadow">Take me home</button></div> }
+                    {index >= quiz.questions.length - 1 && <button onClick={goToHome} className="text-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded shadow">Take me home</button> }
                 </div>
             ))}
         </div>

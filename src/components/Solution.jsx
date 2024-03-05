41e4e27f51bfd39866156ad82f2dbf65c0f4e1f3
Chayan <chayan.surana3@gmail.com>
2024-03-05 22:39:07 +0530

    new changes1

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function Solution({ questions, userAnswers }) {
+export default function Solution({ quiz, userAnswers }) {
     const navigate = useNavigate();
     const goToHome = () => navigate("/");
 
     return (
         <div className="max-w-3xl mx-auto">
-            {questions.map((question, index) => (
+            {quiz.questions.map((question, index) => (
                 <div key={index} className="bg-white shadow-md mt-4 text-black rounded-md p-6 mb-4">
                     <h3 className="text-xl font-semibold mb-4">Question {index + 1}</h3>
                     <h4 className="text-lg mb-4">{question.question}</h4>
@@ -17,7 +17,7 @@ export default function Solution({ questions, userAnswers }) {
                                 key={optionIndex}
                                 className={`block w-full py-2 px-4 rounded ${userAnswers[index] === optionIndex + 1
                                         ? "bg-red-500 text-white"
-                                        : question.correctOption === optionIndex + 1
+                                        : quiz.questions[index].correctOption === optionIndex + 1
                                             ? "bg-green-500 text-white"
                                             : "bg-blue-400 text-black"
                                     }`}

4ae8dab60912c8236f99a1252e780f1c0402f518
Chayan <chayan.surana3@gmail.com>
2024-03-10 11:51:55 +0530

    added google analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-0630VKBG5G');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
